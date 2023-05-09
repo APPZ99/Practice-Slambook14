fa9b304d47a79c1862ae993f3fbb108b94da59de
APPZ99 <appzz99@gmail.com>
2023-05-09 16:12:16 +0800

    ch3

---
+++ Book14/ch3/src/examples/build/CMakeFiles/CoordinateTransform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoordinateTransform.dir/coordinate_transform.cpp.o"
+  "CoordinateTransform.pdb"
+  "CoordinateTransform"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoordinateTransform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Book14/ch3/src/use_eigen/build/CMakeFiles/UseEigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UseEigen.dir/eigen_matrix.cpp.o"
+  "UseEigen.pdb"
+  "UseEigen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UseEigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
