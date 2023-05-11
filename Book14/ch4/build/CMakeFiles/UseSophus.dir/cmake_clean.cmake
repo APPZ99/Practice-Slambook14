4f4947aee6423c3e0b5d9358678d9b73eaffd06e
APPZ99 <appzz99@gmail.com>
2023-05-11 10:22:58 +0800

    ch4

---
+++ Book14/ch4/build/CMakeFiles/UseSophus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UseSophus.dir/use_sophus.cpp.o"
+  "UseSophus.pdb"
+  "UseSophus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UseSophus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
