de7a6dc8b30c9caced36c42965d184761a68bfaf
APPZ99 <appzz99@gmail.com>
2023-05-13 11:30:00 +0800

    ch7

---
+++ Book14/ch7/build/CMakeFiles/orbCv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbCv.dir/orb_cv.cpp.o"
+  "orbCv.pdb"
+  "orbCv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orbCv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Book14/ch7/build/CMakeFiles/orbSelf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbSelf.dir/orb_self.cpp.o"
+  "orbSelf.pdb"
+  "orbSelf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orbSelf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
