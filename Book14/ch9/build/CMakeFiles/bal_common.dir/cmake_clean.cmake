ab0f8b79ac1db58a7ea821bcbccde06bf4604531
APPZ99 <appzz99@gmail.com>
2023-05-14 21:23:37 +0800

    ch9

---
+++ Book14/ch9/build/CMakeFiles/bal_common.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bal_common.dir/src/common.cpp.o"
+  "../lib/libbal_common.pdb"
+  "../lib/libbal_common.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bal_common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
